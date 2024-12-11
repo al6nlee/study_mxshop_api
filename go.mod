7d125c4caf3e8dd356f82e7287e7735a4056c7a1
alan <al6nlee@gmail.com>
2024-12-11 18:09:04 +0800

    add 业务逻辑

--- go.mod
+++ go.mod
@@ -5,6 +5,8 @@ go 1.22.10
 require (
 	github.com/gin-gonic/gin v1.10.0
 	go.uber.org/zap v1.27.0
+	google.golang.org/grpc v1.68.1
+	google.golang.org/protobuf v1.34.2
 )
 
 require (
@@ -29,10 +31,10 @@ require (
 	github.com/ugorji/go/codec v1.2.12 // indirect
 	go.uber.org/multierr v1.10.0 // indirect
 	golang.org/x/arch v0.8.0 // indirect
-	golang.org/x/crypto v0.23.0 // indirect
-	golang.org/x/net v0.25.0 // indirect
-	golang.org/x/sys v0.20.0 // indirect
-	golang.org/x/text v0.15.0 // indirect
-	google.golang.org/protobuf v1.34.1 // indirect
+	golang.org/x/crypto v0.27.0 // indirect
+	golang.org/x/net v0.29.0 // indirect
+	golang.org/x/sys v0.25.0 // indirect
+	golang.org/x/text v0.18.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240903143218-8af14fe29dc1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- user_web/api/user.go
+++ user_web/api/user.go
@@ -1,7 +1,96 @@
 package api
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"net/http"
+	"strconv"
+	reponse "study_mxshop_api/user_web/global/response"
+	"study_mxshop_api/user_web/proto"
+	"time"
+)
+
+func HandleGrpcErrorToHttp(err error, c *gin.Context) {
+	// 将grpc的code转换成http的状态码
+	if err != nil {
+		if e, ok := status.FromError(err); ok {
+			switch e.Code() {
+			case codes.NotFound:
+				c.JSON(http.StatusNotFound, gin.H{
+					"msg": e.Message(),
+				})
+			case codes.Internal:
+				c.JSON(http.StatusInternalServerError, gin.H{
+					"msg:": "内部错误",
+				})
+			case codes.InvalidArgument:
+				c.JSON(http.StatusBadRequest, gin.H{
+					"msg": "参数错误",
+				})
+			case codes.Unavailable:
+				c.JSON(http.StatusInternalServerError, gin.H{
+					"msg": "用户服务不可用",
+				})
+			default:
+				c.JSON(http.StatusInternalServerError, gin.H{
+					"msg": e.Code(),
+				})
+			}
+			return
+		}
+	}
+}
 
 func GetUserList(ctx *gin.Context) {
+	// 拨号连接srv
+	ip := "127.0.0.1"
+	port := 50051
+	userConn, err := grpc.Dial(ip+":"+strconv.Itoa(port), grpc.WithInsecure())
+	if err != nil {
+		zap.S().Errorw("连接srv失败", "err", err)
+		ctx.JSON(200, gin.H{
+			"msg": "连接srv失败",
+		})
+		return
+	}
 
+	// 获取Query传的参数
+	pn := ctx.DefaultQuery("pn", "1")
+	pnInt, _ := strconv.Atoi(pn)
+	pSize := ctx.DefaultQuery("psize", "10")
+	pSizeInt, _ := strconv.Atoi(pSize)
+
+	// 生成grpc的client调用接口
+	userSrvClient := proto.NewUserClient(userConn)
+	rsp, err := userSrvClient.GetUserList(context.Background(), &proto.PageInfo{
+		Pn:    uint32(pnInt),
+		PSize: uint32(pSizeInt),
+	})
+	if err != nil {
+		zap.S().Errorw("获取用户列表失败", "err", err)
+		HandleGrpcErrorToHttp(err, ctx)
+		return
+	}
+
+	reMap := gin.H{
+		"total": rsp.Total,
+	}
+	result := make([]interface{}, 0)
+	for _, value := range rsp.Data {
+		user := reponse.UserResponse{
+			Id:       value.Id,
+			NickName: value.NickName,
+			// Birthday: time.Time(time.Unix(int64(value.BirthDay), 0)).Format("2006-01-02"),
+			Birthday: reponse.JsonTime(time.Unix(int64(value.BirthDay), 0)),
+			Gender:   value.Gender,
+			Mobile:   value.Mobile,
+		}
+		result = append(result, user)
+	}
+	reMap["data"] = result
+	ctx.JSON(http.StatusOK, reMap)
 }
