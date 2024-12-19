03a6801dbba7635527a2e6ff049ccf3adca6e743
alan <al6nlee@gmail.com>
2024-12-19 22:25:52 +0800

    商品服务-商品接口实现

--- go.mod
+++ go.mod
@@ -20,6 +20,7 @@ require (
 	go.uber.org/zap v1.27.0
 	google.golang.org/grpc v1.68.1
 	google.golang.org/protobuf v1.34.2
+	gopkg.in/yaml.v3 v3.0.1
 )
 
 require (
@@ -83,5 +84,4 @@ require (
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20240903143218-8af14fe29dc1 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/natefinch/lumberjack.v2 v2.0.0 // indirect
-	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- goods_web/router/goods.go
+++ goods_web/router/goods.go
@@ -8,9 +8,14 @@ import (
 
 func InitGoodsRouter(Router *gin.RouterGroup) {
 	GoodsRouter := Router.Group("goods")
-	// UserRouter := Router.Group("user").Use(middlewares.JWTAuth())
 	{
-		GoodsRouter.GET("list", goods.List)
-		GoodsRouter.POST("", middlewares.JWTAuth(), middlewares.IsAdminAuth(), goods.New)
+		GoodsRouter.GET("list", goods.List)                                                             // 获取商品列表
+		GoodsRouter.POST("", middlewares.JWTAuth(), middlewares.IsAdminAuth(), goods.New)               // 创建商品
+		GoodsRouter.PUT("/:id", middlewares.JWTAuth(), middlewares.IsAdminAuth(), goods.Update)         // 更新商品
+		GoodsRouter.PATCH("/:id", middlewares.JWTAuth(), middlewares.IsAdminAuth(), goods.UpdateStatus) // 更新商品个别属性状态
+		GoodsRouter.DELETE("/:id", middlewares.JWTAuth(), middlewares.IsAdminAuth(), goods.Delete)      // 删除商品
+
+		GoodsRouter.GET("/:id", goods.Detail)        // 获取商品的详情
+		GoodsRouter.GET("/:id/stocks", goods.Stocks) // 获取商品的库存
 	}
 }
